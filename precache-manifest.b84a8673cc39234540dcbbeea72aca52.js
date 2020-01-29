831707e718e51b32fa4ebb89aea3538ebf9ad1f4
Aleksandra Kulesz <olina.kulesz@gmail.com>
2020-01-29 23:11:33 +0100

    Updates

---
+++ precache-manifest.b84a8673cc39234540dcbbeea72aca52.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cec324982f554f6061001f1ff02f19b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f3577ddc48662fe0fa1e",
+    "url": "/portfolio/static/css/main.01fab9b8.chunk.css"
+  },
+  {
+    "revision": "cc4f1551cf7f7e9bb1fd",
+    "url": "/portfolio/static/js/2.1ffca0dc.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/portfolio/static/js/2.1ffca0dc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f3577ddc48662fe0fa1e",
+    "url": "/portfolio/static/js/main.5f17e438.chunk.js"
+  },
+  {
+    "revision": "e139b2548d94e481a5d0",
+    "url": "/portfolio/static/js/runtime-main.0762ecda.js"
+  },
+  {
+    "revision": "50be5235f7a4f88f7c082dbb26a84444",
+    "url": "/portfolio/static/media/jquery.50be5235.png"
+  },
+  {
+    "revision": "427d568e4c9c1d41e4cab13005c7afad",
+    "url": "/portfolio/static/media/page1.427d568e.png"
+  },
+  {
+    "revision": "dd72077a95f9dd42f63b32fcdcc74ff5",
+    "url": "/portfolio/static/media/page2.dd72077a.png"
+  },
+  {
+    "revision": "259dc2d6b51e6f6952390fcc84385764",
+    "url": "/portfolio/static/media/page3.259dc2d6.png"
+  },
+  {
+    "revision": "2ce25ec503ba6e4acc9aa7bab0d45456",
+    "url": "/portfolio/static/media/splash1-mobile.2ce25ec5.png"
+  },
+  {
+    "revision": "dd99a132869cec0a4d5024104dc1e20c",
+    "url": "/portfolio/static/media/splash1.dd99a132.png"
+  },
+  {
+    "revision": "b3d3a3444486c8ce72045040a3fc985d",
+    "url": "/portfolio/static/media/splash10-mobile.b3d3a344.png"
+  },
+  {
+    "revision": "e5f780743ed328faf4ae473e88c2a72f",
+    "url": "/portfolio/static/media/splash10.e5f78074.png"
+  },
+  {
+    "revision": "6ceaeafcb696d5cfaf6bd41b4b11e779",
+    "url": "/portfolio/static/media/splash2-mobile.6ceaeafc.png"
+  },
+  {
+    "revision": "56e15207196caa71a7e2bded0417597a",
+    "url": "/portfolio/static/media/splash2.56e15207.png"
+  },
+  {
+    "revision": "6cea24facc6b639f2d83494613367700",
+    "url": "/portfolio/static/media/splash3-mobile.6cea24fa.png"
+  },
+  {
+    "revision": "9b54e4725b809a617d5eff8368f509d3",
+    "url": "/portfolio/static/media/splash3.9b54e472.png"
+  },
+  {
+    "revision": "6e71b37a3ad0c05ef6962dc455858a6b",
+    "url": "/portfolio/static/media/splash4-mobile.6e71b37a.png"
+  },
+  {
+    "revision": "37664efc9663feae2ec48e00790d028a",
+    "url": "/portfolio/static/media/splash4.37664efc.png"
+  },
+  {
+    "revision": "4443e8a3df11621c3d8d5d28e5ee90ec",
+    "url": "/portfolio/static/media/splash5-mobile.4443e8a3.png"
+  },
+  {
+    "revision": "c8ff229f21abb800025d2630dc1c5389",
+    "url": "/portfolio/static/media/splash5.c8ff229f.png"
+  },
+  {
+    "revision": "8ef4fd4eff656c9f7ab0ed1729a5ef92",
+    "url": "/portfolio/static/media/splash6-mobile.8ef4fd4e.png"
+  },
+  {
+    "revision": "acfc19cfde7baf6dd8bb3030bfc896c1",
+    "url": "/portfolio/static/media/splash6.acfc19cf.png"
+  },
+  {
+    "revision": "c967c047cac3a2e89093a2f092e39998",
+    "url": "/portfolio/static/media/splash7-mobile.c967c047.png"
+  },
+  {
+    "revision": "47feb0c77c55c6d8317d2ffa0a34b0ce",
+    "url": "/portfolio/static/media/splash7.47feb0c7.png"
+  },
+  {
+    "revision": "c2426b4e49c9daa008c21d3c6027c00a",
+    "url": "/portfolio/static/media/splash8-mobile.c2426b4e.png"
+  },
+  {
+    "revision": "f0be782c8eb713de0112e775b2defce2",
+    "url": "/portfolio/static/media/splash8.f0be782c.png"
+  },
+  {
+    "revision": "df86da23d4f6bd3b6dccca45edeb76fb",
+    "url": "/portfolio/static/media/splash9-mobile.df86da23.png"
+  },
+  {
+    "revision": "80b196239ab24da1794b0130d3be0177",
+    "url": "/portfolio/static/media/splash9.80b19623.png"
+  }
+]);
